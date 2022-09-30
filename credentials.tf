c89b6601e4b8e23c8f99eca5ecfa601afde2fa7c
suraj <surajdk776@gmail.com>
2022-09-29 21:26:22 -0600

    test1

--- credentials.tf
+++ credentials.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "ap-southeast-2"
 access_key = "AKIA6JHQUNJHFPTIQHOE"
-secret_access_key = "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"
+secret_key = "j0DT/iKvLAbXYNVttR/4AQsvVcEkTaMPOBfTwX8q"
 }
