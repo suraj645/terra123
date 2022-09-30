c2fbe7b311cd2b21d748df84a629d478ba711467
suraj <surajdk776@gmail.com>
2022-09-29 22:03:11 -0600

    new

--- credentials.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-region = "ap-southeast-2"
-access_key = "AKIA6JHQUNJHFPTIQHOE"
-secret_key = "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"
-}
--- instance.tf
+++ instance.tf
@@ -1,10 +1,9 @@
- provider "aws" {
-    region = "ap-southeast-2"
-}
+resource "aws_instance" "myweb" {
+  ami           = "ami-067e6178c7a211324"
+  instance_type = "t2.micro"
+  key_name = "sydney"
 
-resource "aws_instance" "webserver" {
-    ami = "ami-067e6178c7a211324"
-    instance_type = "t2.micro"
-    security_groups = ["default"]
-    key_name = "sydney"
+  tags = {
+    Name = "demo"
+  }
 }
