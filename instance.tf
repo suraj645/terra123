c296f29939ca0626f6023b1315a1e8d3d00a072b
suraj <surajdk776@gmail.com>
2022-09-29 21:52:30 -0600

    j

--- instance.tf
+++ instance.tf
@@ -1,9 +1,10 @@
- resource "aws_instance" "test"{
-  ami           = "ami-067e6178c7a211324"
-  instance_type = "t2.micro"
-  key_name = "sydney"
+ provider "aws" {
+    region = "ap-southeast-2"
+}
 
-  tags = {
-    Name = "demo"
-  }
+resource "aws_instance" "webserver" {
+    ami = "ami-067e6178c7a211324"
+    instance_type = "t2.micro"
+    security_groups = ["default"]
+    key_name = "sydney"
 }
