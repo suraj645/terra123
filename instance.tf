e3ef16d5c02510938e65a70c941dbe7ea3bf456a
suraj <surajdk776@gmail.com>
2022-10-07 22:03:43 -0600

    changwd

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myweb" {
   ami           = "ami-067e6178c7a211324"
   instance_type = "t2.micro"
-  key_name = "sydney"
+  key_name = "sydny"
 
   tags = {
     Name = "demo"
